16786c76252aa1725586caf65b1a2ec679d854d6
Naytitorn Chaovirachot <naytitorn21@gmail.com>
2025-04-15 23:12:12 +0700

    extract user name + email from firebase

--- app/src/main/java/org/classapp/sleepwell/screens/ProfileScreen.kt
+++ app/src/main/java/org/classapp/sleepwell/screens/ProfileScreen.kt
@@ -1,5 +1,8 @@
 package org.classapp.sleepwell.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -9,15 +12,37 @@ import org.classapp.sleepwell.navigation.Routes
 
 @Composable
 fun ProfileScreen(navController: NavController) {
-    Text(
-        text = "Profile Screen",
-    )
-    Button(onClick = {
-        FirebaseAuth.getInstance().signOut()
-        navController.navigate(Routes.SIGN_IN) {
-            popUpTo(Routes.MAIN) { inclusive = true }
+    val user = FirebaseAuth.getInstance().currentUser
+
+    if (user != null) {
+        val displayName = user.displayName ?: "No Username"
+        val email = user.email ?: "No Email"
+        val photoUrl = user.photoUrl?.toString()
+
+        // Displaying info in Compose UI
+        Column {
+            Text(text = "Username: $displayName")
+            Text(text = "Email: $email")
+            Text(
+                text = "Profile Screen",
+            )
+            Button(onClick = {
+                FirebaseAuth.getInstance().signOut()
+                navController.navigate(Routes.SIGN_IN) {
+                    popUpTo(Routes.MAIN) { inclusive = true }
+                }
+            }) {
+                Text("Logout")
+            }
         }
-    }) {
-        Text("Logout")
+
+//        if (photoUrl != null) {
+//            Image(
+//                painter = rememberAsyncImagePainter(photoUrl),
+//                contentDescription = "Profile Picture"
+//            )
+//        } else {
+//            Text(text = "No Profile Picture")
+//        }
     }
 }
