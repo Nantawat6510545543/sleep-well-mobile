b4683aec72d1c105d4d376d16fa403451a3e9c92
Naytitorn Chaovirachot <naytitorn21@gmail.com>
2025-04-15 22:36:44 +0700

    make login persistent

--- app/src/main/java/org/classapp/sleepwell/screens/AuthenticationScreen.kt
+++ app/src/main/java/org/classapp/sleepwell/screens/AuthenticationScreen.kt
@@ -1,11 +1,13 @@
 package org.classapp.sleepwell.screens
 
+import android.app.Activity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.google.firebase.auth.FirebaseAuth
 
 
 @Composable
@@ -17,17 +19,28 @@ fun AuthenticationScreen(
         onResult = onSignInResult
     )
 
-    val providers = arrayListOf(
-        AuthUI.IdpConfig.EmailBuilder().build(),
-        AuthUI.IdpConfig.GoogleBuilder().build()
-    )
-
-    val intent = AuthUI.getInstance()
-        .createSignInIntentBuilder()
-        .setAvailableProviders(providers)
-        .build()
+    val currentUser = FirebaseAuth.getInstance().currentUser
 
     LaunchedEffect(true) {
-        launcher.launch(intent)
+        if (currentUser != null) {
+            // User is already signed in, simulate a success result
+            onSignInResult(
+                FirebaseAuthUIAuthenticationResult(
+                    Activity.RESULT_OK, null
+                )
+            )
+        } else {
+            val providers = arrayListOf(
+                AuthUI.IdpConfig.EmailBuilder().build(),
+                AuthUI.IdpConfig.GoogleBuilder().build()
+            )
+
+            val intent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build()
+
+            launcher.launch(intent)
+        }
     }
 }
